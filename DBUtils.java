a089007679582eb022d16c3f57296583308ad7a9
thresh123 <92571569+thresh123@users.noreply.github.com>
2021-12-23 11:00:31 +0800

    Add files via upload

---
+++ DBUtils.java
@@ -0,0 +1,22 @@
+package cn.itcast.notepad;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DBUtils {
+    public static final String DATABASE_NAME = "Notepad";
+    public static final String DATABASE_TABLE = "Note";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String NOTEPAD_ID = "id";
+    public static final String NOTEPAD_CONTENT = "content";
+    public static final String NOTEPAD_TIME = "time";
+
+    public static final String getTime(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        return simpleDateFormat.format(date);
+    }
+
+}
+
